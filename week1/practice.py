913a955c2df40e76c607dd8b790e7502bbb4504b
khus759 <khushikumarigupta@email.com>
2024-07-13 17:50:29 +0530

    practice1

---
+++ week1/practice.py
@@ -0,0 +1,7 @@
+square=[1,4,9,16,25,36,49,64,81,100]
+square[2]=50
+print(square,("replace new element"))
+square.insert(5,25)
+print(square,("insert new element"))   
+square.pop()
+print(square,("remove last element"))
