30def6e018f25e00e648f8b1e6391633e26de366
khus759 <khushikumarigupta@email.com>
2024-07-15 15:47:33 +0530

    practice 8

---
+++ week1/practice8.py
@@ -0,0 +1,9 @@
+primenum={1,2,3,5,7,}
+print("First five prime number:-",primenum)
+primenum.add(11)
+print("Add number 11:-",primenum)
+primenum.remove(2)
+print("Remove number 2:-",primenum)
+number=7 in primenum
+print("Number 7 in set:-",number)
+print("Final set",primenum)
