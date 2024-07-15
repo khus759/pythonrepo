8e4a7483a915a0c2c44b639b94b4783b848505f0
khus759 <khushikumarigupta@email.com>
2024-07-15 16:37:51 +0530

    practice 4

---
+++ week1/practice4.py
@@ -0,0 +1,5 @@
+continents=("Asia","America","Europe","Antatica","Africa")
+print("Five continents:-",continents)
+print("Third continent in the tuple:-",continents[2])
+print("error tulpe can't change")
+
