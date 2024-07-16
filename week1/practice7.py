209ae1ea28556fc685f7a6e07afbaf0a8866983b
khus759 <khushikumarigupta@email.com>
2024-07-16 16:03:36 +0530

    practice7

---
+++ week1/practice7.py
@@ -0,0 +1,12 @@
+print("odd number :- ")
+odd={1,3,5,7,9}
+print(odd)
+print("even number :- ")
+even={2,4,6,8,10}
+print(even)
+Union_set=odd.union(even)
+intersection_set=odd.intersection(even)
+print("Union Set :- ")
+print(Union_set)
+print("Intersection Set :- ")
+print(intersection_set)
