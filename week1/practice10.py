daedf7a8a17d7554bb74f13e643ab4fda1847ac1
khus759 <khushikumarigupta@email.com>
2024-07-16 16:04:16 +0530

    practice10

---
+++ week1/practice10.py
@@ -0,0 +1,10 @@
+dic={ "student" : "Khushi" ,
+     "age" : 18 ,
+     "grade" : "A"
+}
+print("dictionary :- ",dic)
+dic.update({"age":19})
+print("update dictionary :- ",dic)
+dic.update({"Subject" : "English"})
+print("Add a new key-value pair :- ",dic)
+
